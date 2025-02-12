5f171d6b90273ccf4d5d404f2a9e84cfd8278b0c
saschakrischock <s.krischock@gmail.com>
2025-02-12 12:37:17 +0200

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,15 +6,27 @@ export default {
         headline: ['ABC Diatype Unlicensed Trial'],
       },
       fontSize: {
-        lg: ['0.938rem', '1rem', {
-          letterSpacing: '0',
+        lg: ['0.938rem', {
+          lineHeight: '1rem',
+          letterSpacing: '0'
         }],
-        xl: ['1.6rem', '1.8rem', {
-            letterSpacing: '0',
-          }],
-        'mobile-xl':  ['1.25rem', '1.4rem'], 
-        '2xl': ['5rem', '4.8rem'],
-        'mobile-2xl':  ['2.2rem', '2.3rem'],
+        xl: ['1.6rem', {
+          lineHeight: '1.8rem',
+          letterSpacing: '-0.03rem'
+        }],
+        'mobile-xl': ['1.25rem', {
+          lineHeight: '1.4rem',
+          letterSpacing: '-0.02rem'
+        }],
+        '2xl': ['5rem', {
+          lineHeight: '4.8rem',
+          letterSpacing: '-0.15rem',
+          fontWeight: 700
+        }],
+        'mobile-2xl': ['2.2rem', {
+          lineHeight: '2.3rem',
+          letterSpacing: '-0.02rem'
+        }]
       }
     }
   }
