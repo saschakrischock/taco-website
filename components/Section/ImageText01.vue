0cff2ae90a764246e53a03a3049db70bee88470d
saschakrischock <s.krischock@gmail.com>
2025-02-21 10:49:28 +0100

    text

--- components/Section/ImageText01.vue
+++ components/Section/ImageText01.vue
@@ -49,6 +49,15 @@
                <br> facility with a shadowy data harvestor – would see nothing but ciphertexts.
             </h3>
         </div>
+<<<<<<< HEAD
+=======
+
+        
+
+
+
+
+>>>>>>> c6031de (text)
     </div>
   </section>
 </template>
