9f60d6e937ef5a9c6a5147f200cf551672b3db7f
saschakrischock <s.krischock@gmail.com>
2025-02-23 15:04:18 +0100

    changes

--- components/Section/Hero.vue
+++ components/Section/Hero.vue
@@ -77,10 +77,14 @@
         <h3 class="subtitle font-headline mt-6 text-black font-medium text-mobile-xl lg:text-xl">
           TACo is the only general-purpose access control plugin that works without <br class="max-lg:hidden">
           
+<<<<<<< HEAD
           trusting an intermediary. So users aren't obliged to trust:  
 >>>>>>> 92e046e (changes)
+=======
+          trusting an intermediary. So users aren't obliged to trust
+>>>>>>> 99f8c8c (changes)
           <span :class="['transition-colors duration-300', { 'text-[#96FF5E]': isFirstGreen }]"> AWS KMS, </span>
-          <span :class="['transition-colors duration-300', { 'text-[#96FF5E]': isSecondGreen }]">Your dev team </span>
+          <span :class="['transition-colors duration-300', { 'text-[#96FF5E]': isSecondGreen }]">Your dev<br> team </span>
           <span :class="['transition-colors duration-300', { 'text-[#96FF5E]': isThirdGreen }]">or TACo</span>
         </h3>
       </div>
