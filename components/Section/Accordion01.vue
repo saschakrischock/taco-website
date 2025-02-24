8cafd6f3de7796dcdb4e3205150addfcbd98834c
saschakrischock <s.krischock@gmail.com>
2025-02-24 22:05:15 +0100

    access

--- components/Section/Accordion01.vue
+++ components/Section/Accordion01.vue
@@ -146,7 +146,7 @@ const items = ref([
   },
   {
     title: 'Decentralized GenAI',
-    content: 'Communication with or between LLM models should be 100% private and censorship-resistant, not mined by intermediaries or blocked by a central authority. TACo can be harnessed for e2ee inference, acess-controlled agentic RAG, and distributed computation.',
+    content: 'Communication with or between LLM models should be 100% private and censorship-resistant, not mined by intermediaries or blocked by a central authority. TACo can be harnessed for e2ee inference, access-controlled agentic RAG, and distributed computation.',
     icon: Ai
   },
   {
