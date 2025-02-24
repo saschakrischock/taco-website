3600095a1dd6feea76a249e68bcd316960a7332d
saschakrischock <s.krischock@gmail.com>
2025-02-25 00:03:48 +0100

    changes

--- components/Section/ImageText01.vue
+++ components/Section/ImageText01.vue
@@ -1,9 +1,4 @@
 <template>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 6d1e519 (changes)
   <section ref="container" class="hero is-primary bg-black min-h-[100vh] is-fullheight relative overflow-hidden">
     <div 
       ref="parallaxBg"
@@ -21,15 +16,7 @@
         <h2 class="text-white lg:text-2xl text-mobile-2xl font-bold font-headline">
           End-to-end encryption<br> for (almost) everything.
         </h2>
-<<<<<<< HEAD
-<<<<<<< HEAD
         <h3 class="subtitle font-headline text-white max-w-[60rem] font-bold text-mobile-xl lg:text-mobile-xl lg:text-xl mt-7">
-=======
-        <h3 class="subtitle font-headline text-white max-w-[60rem] font-medium text-mobile-xl lg:text-mobile-xl lg:text-xl mt-7">
->>>>>>> 6d1e519 (changes)
-=======
-        <h3 class="subtitle font-headline text-white max-w-[60rem] font-bold text-mobile-xl lg:text-mobile-xl lg:text-xl mt-7">
->>>>>>> 344f71b (changes)
           Imagine if Signal-like protections were embedded in every application.<br>
           So 
           <span class="green-highlight">healthcare portals, social platforms, shared spreadsheets, booking<br> aggregators, connected vehicles, chatbot providers
@@ -37,72 +24,6 @@
           <br> facility with a shadowy data harvestor – would see nothing but ciphertexts.
         </h3>
       </div>
-<<<<<<< HEAD
-    <section ref="container" class="hero is-primary bg-black min-h-[100svh] is-fullheight relative overflow-hidden">
-=======
-    <section ref="container" class="hero is-primary bg-black min-h-[100dvh] is-fullheight relative overflow-hidden">
->>>>>>> 69537b9 (changes)
-=======
-    <section ref="container" class="hero is-primary bg-black min-h-[100vh] is-fullheight relative overflow-hidden">
->>>>>>> 8e05bf8 (changes)
-      <div 
-        ref="parallaxBg"
-        class="absolute scale-fix inset-0 w-full h-[100%] top-[0%] bg-center bg-cover bg-no-repeat"
-        :style="{
-          backgroundImage: 'url(/images/1.jpg)',
-          transform: `translate3d(0, ${parallaxOffset}px, 0)`,
-          zIndex: 0
-        }"
-      ></div>
-      <div class="absolute h-72 inset-0 w-full  bg-gradient-to-b from-black to-transparent z-10"></div>
-      <div class="absolute h-72 bottom-0  w-full bg-gradient-to-t from-black to-transparent z-10"></div>
-      <div class="hero-body absolute z-20">
-        <div class="lg:p-7 p-4 max-w-[90rem]  h-[calc(100vh-1rem)]  lg:h-[calc(100vh-3rem)] font-headline  flex justify-between flex-col">
-            <h2 class="text-white   lg:text-2xl text-mobile-2xl font-bold font-headline">
-              End-to-end encryption<br> for (almost) everything.
-            </h2>
-            <h3 class="subtitle font-headline text-white max-w-[60rem]  font-medium text-mobile-xl lg:text-mobile-xl lg:text-xl mt-7">
-              Imagine if Signal-like protections were embedded in every application.<br>
-              So 
-              <span class="green-highlight">healthcare portals, social platforms, shared spreadsheets, booking<br> aggregators, connected vehicles, chatbot providers
-               </span> – and every other digital
-               <br> facility with a shadowy data harvestor – would see nothing but ciphertexts.
-            </h3>
-        </div>
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-
-=======
->>>>>>> 2792660 (YOUR_COMMIT_MESSAGE)
-
-=======
-=======
->>>>>>> b844fae (text)
-
-        
-
-
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
-        
->>>>>>> 54ade92 (changes)
-=======
->>>>>>> 2c89bc0 (YOUR_COMMIT_MESSAGE)
-=======
->>>>>>> d088444 (Update website content and styling)
-=======
-
-
->>>>>>> c6031de (text)
->>>>>>> b844fae (text)
-=======
->>>>>>> 19b76fe (changes)
-=======
->>>>>>> 6d1e519 (changes)
     </div>
   </section>
 </template>
